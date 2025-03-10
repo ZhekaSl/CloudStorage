d8e117b98e8df3e50fa8dc9736012542d5eb7c59
Zheka Slepko <zhenya.slepko@gmail.com>
2025-03-10 15:04:38 +0200

    feat: create user entity.

---
+++ src/main/java/ua/zhenya/cloudstorage/model/User.java
@@ -0,0 +1,42 @@
+package ua.zhenya.cloudstorage.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Table(name = "users", indexes = {
+        @Index(name = "idx_username", columnList = "username", unique = true)
+})
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(unique = true, nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getEffectiveClass(this) != getEffectiveClass(obj)) return false;
+        return getId() != null && getId().equals(((User) obj).getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return getEffectiveClass(this).hashCode();
+    }
+
+    private static Class<?> getEffectiveClass(Object o) {
+        return o instanceof HibernateProxy
+                ? ((HibernateProxy) o).getHibernateLazyInitializer().getClass()
+                : o.getClass();
+    }
+}
