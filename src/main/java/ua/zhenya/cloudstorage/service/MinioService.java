87fc54adb587f07eb6869e6a2aa4b4efc763f1a9
Zheka Slepko <zhenya.slepko@gmail.com>
2025-03-11 19:50:02 +0200

    feat: implement methods for MinioService.

--- src/main/java/ua/zhenya/cloudstorage/service/MinioService.java
+++ src/main/java/ua/zhenya/cloudstorage/service/MinioService.java
@@ -1,6 +1,27 @@
 package ua.zhenya.cloudstorage.service;
 
+import io.minio.GetObjectResponse;
+import io.minio.ObjectWriteResponse;
+import io.minio.StatObjectResponse;
+import io.minio.errors.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
 public interface MinioService {
-    void createDirectory(String fullPath);
-    void createBucket();
+    void createDirectory(String fullPath) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException;
+
+    GetObjectResponse getObject(String fullPath) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException;
+
+    void deleteObject(String fullPath) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException;
+
+    ObjectWriteResponse uploadObject(String path, InputStream inputStream, long size, String contentType) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException;
+
+    StatObjectResponse getObjectInfo(String path) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException;
+
+    void copyObject(String sourceObjectPath, String targetObjectPath) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException;
+
+    void moveObject(String sourceObjectPath, String targetObjectPath) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException;
 }
--- src/main/java/ua/zhenya/cloudstorage/service/impl/MinioServiceImpl.java
+++ src/main/java/ua/zhenya/cloudstorage/service/impl/MinioServiceImpl.java
@@ -1,9 +1,7 @@
 package ua.zhenya.cloudstorage.service.impl;
 
-import io.minio.BucketExistsArgs;
-import io.minio.MakeBucketArgs;
-import io.minio.MinioClient;
-import io.minio.PutObjectArgs;
+import io.minio.*;
+import io.minio.errors.*;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,7 +9,10 @@ import ua.zhenya.cloudstorage.properties.MinioProperties;
 import ua.zhenya.cloudstorage.service.MinioService;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 @Service
 @RequiredArgsConstructor
@@ -20,34 +21,73 @@ public class MinioServiceImpl implements MinioService {
     private final MinioProperties minioProperties;
 
     @PostConstruct
-    public void init() {
+    public void init() throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
         createBucket();
     }
 
     @Override
-    public void createBucket() {
+    public void createDirectory(String fullPath) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+        minioClient.putObject(PutObjectArgs.builder()
+                .bucket(minioProperties.getBucketName())
+                .object(fullPath)
+                .stream(new ByteArrayInputStream(new byte[0]), 0, -1)
+                .build());
+    }
+
+    public GetObjectResponse getObject(String fullPath) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+        return minioClient.getObject(GetObjectArgs.builder()
+                .bucket(minioProperties.getBucketName())
+                .object(fullPath)
+                .build());
+    }
+
+    public void deleteObject(String fullPath) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+        minioClient.removeObject(RemoveObjectArgs.builder()
+                .bucket(minioProperties.getBucketName())
+                .object(fullPath)
+                .build());
+    }
+
+    public ObjectWriteResponse uploadObject(String path, InputStream inputStream, long size, String contentType) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+        return minioClient.putObject(PutObjectArgs.builder()
+                .bucket(minioProperties.getBucketName())
+                .object(path)
+                .stream(inputStream, size, -1)
+                .contentType(contentType)
+                .build());
+    }
+
+    public StatObjectResponse getObjectInfo(String path) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+        return minioClient.statObject(StatObjectArgs.builder()
+                .bucket(minioProperties.getBucketName())
+                .object(path)
+                .build());
+    }
+
+    public void copyObject(String sourceObjectPath, String targetObjectPath) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+        minioClient.copyObject(CopyObjectArgs.builder()
+                .source(CopySource.builder()
+                        .bucket(minioProperties.getBucketName())
+                        .object(sourceObjectPath)
+                        .build())
+                .bucket(minioProperties.getBucketName())
+                .object(targetObjectPath)
+                .build());
+    }
+
+    public void moveObject(String sourceObjectPath, String targetObjectPath) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+        copyObject(sourceObjectPath, targetObjectPath);
+        deleteObject(sourceObjectPath);
+    }
+
+    private void createBucket() throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
         String bucketName = minioProperties.getBucketName();
 
-        try {
-            boolean bucketExists = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
-            if (!bucketExists) {
-                minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
-            }
-        } catch (Exception e) {
-            System.out.println("Something went wrong: " + e.getMessage());
+        boolean bucketExists = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
+        if (!bucketExists) {
+            minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
         }
     }
 
-    @Override
-    public void createDirectory(String fullPath) {
-        try (InputStream inputStream = new ByteArrayInputStream(new byte[0])) {
-            minioClient.putObject(PutObjectArgs.builder()
-                    .bucket(minioProperties.getBucketName())
-                    .object(fullPath)
-                    .stream(inputStream, 0, -1)
-                    .build());
-        } catch (Exception e) {
-            System.out.println("Something went wrong: " + e.getMessage());
-        }
-    }
+
 }
