8f30ac76702d5c7a35744921ca6d4049fd0fa9f9
Zheka Slepko <zhenya.slepko@gmail.com>
2025-03-13 14:21:11 +0200

    feat: add ResourceService.uploadResources.

--- src/main/java/ua/zhenya/cloudstorage/service/ResourceService.java
+++ src/main/java/ua/zhenya/cloudstorage/service/ResourceService.java
@@ -1,11 +1,14 @@
 package ua.zhenya.cloudstorage.service;
 
+import org.springframework.web.multipart.MultipartFile;
 import ua.zhenya.cloudstorage.dto.ResourceResponse;
 
 import java.io.InputStream;
 import java.util.List;
 
 public interface ResourceService {
+    List<ResourceResponse> uploadResources(Integer userId, String path, MultipartFile[] files);
+
     ResourceResponse getResourceInfo(Integer userId, String path);
 
     ResourceResponse createDirectory(Integer userId, String path);
@@ -19,5 +22,4 @@ public interface ResourceService {
     ResourceResponse moveResource(Integer userId, String from, String to);
 
     List<ResourceResponse> searchResources(Integer userId, String query);
-
 }
