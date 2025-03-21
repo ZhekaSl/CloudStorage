a28a05905920a6c7283c1cae87a748b775caa9db
Zheka Slepko <zhenya.slepko@gmail.com>
2025-03-21 18:37:11 +0200

    refactor: add @BeforeAll and @AfterEach to BaseTest.

--- src/test/java/ua/zhenya/cloudstorage/BaseTest.java
+++ src/test/java/ua/zhenya/cloudstorage/BaseTest.java
@@ -22,6 +22,7 @@ import org.springframework.web.multipart.MultipartFile;
 import org.testcontainers.containers.MinIOContainer;
 import ua.zhenya.cloudstorage.service.MinioService;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
@@ -32,6 +33,7 @@ import java.util.function.Function;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static ua.zhenya.cloudstorage.testdata.TestConstants.JPEG;
+import static ua.zhenya.cloudstorage.testdata.TestConstants.USER_DIRECTORY_PATH;
 
 @ActiveProfiles("test")
 @SqlGroup({
@@ -44,13 +46,17 @@ import static ua.zhenya.cloudstorage.testdata.TestConstants.JPEG;
                 executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD
         )
 })
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public abstract class BaseTest {
     static final MinIOContainer minioContainer = new MinIOContainer("minio/minio")
             .withUserName("username")
             .withPassword("password");
 
     @Autowired
-    private MinioClient minioClient;
+    protected MinioClient minioClient;
+
+    @Autowired
+    protected MinioService minioService;
 
     @Value("${minio.bucket-name}")
     private String bucketName;
@@ -66,28 +72,38 @@ public abstract class BaseTest {
         registry.add("minio.secret-key", minioContainer::getPassword);
     }
 
-    @BeforeEach
-    void setUp() throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+    @BeforeAll
+    void prepareMinio() throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
         boolean found = minioClient.bucketExists(
                 BucketExistsArgs.builder().bucket(bucketName).build()
         );
-
         if (!found) {
             minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
+        }
+
+        boolean objectExists = minioService.objectExists(USER_DIRECTORY_PATH);
+        if (!objectExists) {
+            minioClient.putObject(PutObjectArgs.builder()
+                    .bucket(bucketName)
+                    .object(USER_DIRECTORY_PATH)
+                    .stream(new ByteArrayInputStream(new byte[0]), 0, -1)
+                    .build());
         }
     }
 
     @AfterEach
     void clearMinio() throws Exception {
         Iterable<Result<Item>> results = minioClient.listObjects(
-                ListObjectsArgs.builder().bucket(bucketName).recursive(true).build()
+                ListObjectsArgs.builder().bucket(bucketName).prefix(USER_DIRECTORY_PATH).recursive(true).build()
         );
 
         for (Result<Item> result : results) {
             String objectName = result.get().objectName();
-            minioClient.removeObject(
-                    RemoveObjectArgs.builder().bucket(bucketName).object(objectName).build()
-            );
+            if (!objectName.equals(USER_DIRECTORY_PATH)) {
+                minioClient.removeObject(
+                        RemoveObjectArgs.builder().bucket(bucketName).object(objectName).build()
+                );
+            }
         }
     }
 
