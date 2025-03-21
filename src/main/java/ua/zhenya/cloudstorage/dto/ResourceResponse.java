d3f074bfca8d8127b36f8dd4b1277e38a449c339
Zheka Slepko <zhenya.slepko@gmail.com>
2025-03-21 14:51:21 +0200

    feat: configure ResourceResponse.

--- src/main/java/ua/zhenya/cloudstorage/dto/ResourceResponse.java
+++ src/main/java/ua/zhenya/cloudstorage/dto/ResourceResponse.java
@@ -1,10 +1,12 @@
 package ua.zhenya.cloudstorage.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 @Builder
 public class ResourceResponse {
     private String path;
