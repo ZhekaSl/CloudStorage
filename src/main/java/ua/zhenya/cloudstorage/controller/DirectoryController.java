7d24ff4c19d95cd8183044bb88a4838112cf7f38
Zheka Slepko <zhenya.slepko@gmail.com>
2025-03-23 00:16:32 +0200

    feat: add validation for 'path' in controllers.

--- src/main/java/ua/zhenya/cloudstorage/controller/DirectoryController.java
+++ src/main/java/ua/zhenya/cloudstorage/controller/DirectoryController.java
@@ -1,23 +1,31 @@
 package ua.zhenya.cloudstorage.controller;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import ua.zhenya.cloudstorage.dto.DirectoryResponse;
-import ua.zhenya.cloudstorage.service.DirectoryService;
+import org.springframework.web.bind.annotation.*;
+import ua.zhenya.cloudstorage.dto.ResourceResponse;
+import ua.zhenya.cloudstorage.service.ResourceService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/directory")
 public class DirectoryController {
-    private final DirectoryService directoryService;
+    private final ResourceService resourceService;
 
     @PostMapping
-    public ResponseEntity<DirectoryResponse> createDirectory(Long id, @RequestParam String path) {
-
-
+    public ResponseEntity<ResourceResponse> createDirectory(@RequestParam Integer id, @RequestParam @NotBlank String path) {
+        ResourceResponse response = resourceService.createDirectory(id, path);
+        return ResponseEntity.status(HttpStatus.CREATED.value()).body(response);
     }
+
+    @GetMapping
+    public ResponseEntity<List<ResourceResponse>> getDirectoryContext(@RequestParam Integer id, @RequestParam @NotBlank String path) {
+        return ResponseEntity.ok(resourceService.getDirectoryContent(id, path));
+    }
+
+
 }
--- src/main/java/ua/zhenya/cloudstorage/controller/ResourceController.java
+++ src/main/java/ua/zhenya/cloudstorage/controller/ResourceController.java
@@ -1,47 +1,52 @@
 package ua.zhenya.cloudstorage.controller;
 
-import lombok.Getter;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.*;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import ua.zhenya.cloudstorage.dto.ResourceDownloadResponse;
 import ua.zhenya.cloudstorage.dto.ResourceResponse;
 import ua.zhenya.cloudstorage.service.impl.ResourceServiceImpl;
 
-import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Paths;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/resource")
+@Validated
 public class ResourceController {
     private final ResourceServiceImpl resourceService;
 
     @GetMapping
-    public ResponseEntity<ResourceResponse> getResource(@RequestParam Integer id, @RequestParam String path) {
+    public ResponseEntity<ResourceResponse> getResource(@RequestParam Integer id, @NotBlank @RequestParam String path) {
         ResourceResponse resourceInfo = resourceService.getResourceInfo(id, path);
         return new ResponseEntity<>(resourceInfo, HttpStatus.OK);
     }
 
     @PostMapping
-    public ResponseEntity<List<ResourceResponse>> uploadResource(@RequestParam(value = "userId") Integer id, @RequestParam String path, @RequestParam MultipartFile[] files) {
+    public ResponseEntity<List<ResourceResponse>> uploadResource(@RequestParam(value = "userId") Integer id,
+                                                                 @RequestParam @NotBlank String path,
+                                                                 @RequestParam MultipartFile[] files) {
         List<ResourceResponse> resourceResponses = resourceService.uploadResources(id, path, files);
         return ResponseEntity.status(HttpStatus.CREATED.value()).body(resourceResponses);
     }
 
     @DeleteMapping
-    public ResponseEntity<?> deleteResource(Integer id, @RequestParam String path) {
+    public ResponseEntity<?> deleteResource(Integer id, @RequestParam @NotBlank String path) {
         resourceService.deleteResource(id, path);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/download")
-    public ResponseEntity<Resource> downloadResource(@RequestParam Integer id, @RequestParam String path) {
+    public ResponseEntity<Resource> downloadResource(@RequestParam Integer id, @RequestParam @NotBlank String path) {
         ResourceDownloadResponse resource = resourceService.downloadResource(id, path);
         String fileName = resource.getFileName();
 
@@ -56,13 +61,16 @@ public class ResourceController {
     }
 
     @GetMapping("/move")
-    public ResponseEntity<ResourceResponse> moveResource(@RequestParam Integer id, @RequestParam String from, @RequestParam String to) {
+    public ResponseEntity<ResourceResponse> moveResource(@RequestParam Integer id,
+                                                         @RequestParam @NotBlank String from,
+                                                         @RequestParam @NotBlank String to) {
         ResourceResponse response = resourceService.moveResource(id, from, to);
         return ResponseEntity.ok().body(response);
     }
 
     @GetMapping("/search")
-    public ResponseEntity<List<ResourceResponse>> searchResource(@RequestParam Integer id, @RequestParam String query) {
+    public ResponseEntity<List<ResourceResponse>> searchResource(@RequestParam Integer id,
+                                                                 @RequestParam @NotBlank String query) {
         return ResponseEntity.ok(resourceService.searchResources(id, query));
     }
 }
