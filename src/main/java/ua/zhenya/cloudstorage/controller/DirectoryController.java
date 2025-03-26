89e4c8b230a627be6d9be250622285046b6eb1b1
Zheka Slepko <zhenya.slepko@gmail.com>
2025-03-26 17:09:53 +0200

    feat: add logging to resource controllers.

--- src/main/java/ua/zhenya/cloudstorage/controller/DirectoryController.java
+++ src/main/java/ua/zhenya/cloudstorage/controller/DirectoryController.java
@@ -2,9 +2,12 @@ package ua.zhenya.cloudstorage.controller;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import ua.zhenya.cloudstorage.config.UserDetailsImpl;
 import ua.zhenya.cloudstorage.dto.ResourceResponse;
 import ua.zhenya.cloudstorage.service.ResourceService;
 
@@ -13,19 +16,25 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/directory")
+@Slf4j
 public class DirectoryController {
     private final ResourceService resourceService;
 
     @PostMapping
-    public ResponseEntity<ResourceResponse> createDirectory(@RequestParam Integer id, @RequestParam @NotBlank String path) {
-        ResourceResponse response = resourceService.createDirectory(id, path);
+    public ResponseEntity<ResourceResponse> createDirectory(@AuthenticationPrincipal UserDetailsImpl userDetailsImpl,
+                                                            @RequestParam @NotBlank String path) {
+        Integer userId = userDetailsImpl.getId();
+        log.info("Received POST /api/directory request for user ID: {}, path '{}'", userId, path);
+
+        ResourceResponse response = resourceService.createDirectory(userDetailsImpl.getId(), path);
         return ResponseEntity.status(HttpStatus.CREATED.value()).body(response);
     }
 
     @GetMapping
-    public ResponseEntity<List<ResourceResponse>> getDirectoryContext(@RequestParam Integer id, @RequestParam @NotBlank String path) {
-        return ResponseEntity.ok(resourceService.getDirectoryContent(id, path));
+    public ResponseEntity<List<ResourceResponse>> getDirectoryContext(@AuthenticationPrincipal UserDetailsImpl userDetailsImpl,
+                                                                      @RequestParam String path) {
+        Integer userId = userDetailsImpl.getId();
+        log.info("Received GET /api/directory request for user ID: {}, path '{}'", userId, path);
+        return ResponseEntity.ok(resourceService.getDirectoryContent(userDetailsImpl.getId(), path));
     }
-
-
 }
--- src/main/java/ua/zhenya/cloudstorage/controller/ResourceController.java
+++ src/main/java/ua/zhenya/cloudstorage/controller/ResourceController.java
@@ -1,16 +1,15 @@
 package ua.zhenya.cloudstorage.controller;
 
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.Resource;
 import org.springframework.http.*;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import ua.zhenya.cloudstorage.config.UserDetailsImpl;
 import ua.zhenya.cloudstorage.dto.ResourceDownloadResponse;
 import ua.zhenya.cloudstorage.dto.ResourceResponse;
 import ua.zhenya.cloudstorage.service.impl.ResourceServiceImpl;
@@ -22,32 +21,44 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/api/resource")
 @Validated
+@Slf4j
 public class ResourceController {
     private final ResourceServiceImpl resourceService;
 
     @GetMapping
-    public ResponseEntity<ResourceResponse> getResource(@RequestParam Integer id, @NotBlank @RequestParam String path) {
-        ResourceResponse resourceInfo = resourceService.getResourceInfo(id, path);
+    public ResponseEntity<ResourceResponse> getResource(@AuthenticationPrincipal UserDetailsImpl userDetailsImpl,
+                                                        @RequestParam @NotBlank(message = "'path' must not be blank") String path) {
+        ResourceResponse resourceInfo = resourceService.getResourceInfo(userDetailsImpl.getId(), path);
+        log.info("Received GET /api/resource request for user ID: {} and path: '{}'", userDetailsImpl.getId(), path);
         return new ResponseEntity<>(resourceInfo, HttpStatus.OK);
     }
 
-    @PostMapping
-    public ResponseEntity<List<ResourceResponse>> uploadResource(@RequestParam(value = "userId") Integer id,
-                                                                 @RequestParam @NotBlank String path,
-                                                                 @RequestParam MultipartFile[] files) {
-        List<ResourceResponse> resourceResponses = resourceService.uploadResources(id, path, files);
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<List<ResourceResponse>> uploadResource(@AuthenticationPrincipal UserDetailsImpl userDetailsImpl,
+                                                                 @RequestParam String path,
+                                                                 @RequestPart("object") List<MultipartFile> files) {
+        Integer userId = userDetailsImpl.getId();
+        int fileCount = (files != null) ? files.size() : 0;
+        log.info("Received POST /api/resource request for user ID: {} to path: '{}' with {} file(s)", userId, path, fileCount);
+        List<ResourceResponse> resourceResponses = resourceService.uploadResources(userDetailsImpl.getId(), path, files);
         return ResponseEntity.status(HttpStatus.CREATED.value()).body(resourceResponses);
     }
 
     @DeleteMapping
-    public ResponseEntity<?> deleteResource(Integer id, @RequestParam @NotBlank String path) {
-        resourceService.deleteResource(id, path);
+    public ResponseEntity<?> deleteResource(@AuthenticationPrincipal UserDetailsImpl userDetailsImpl,
+                                            @RequestParam @NotBlank(message = "'path' must not be blank") String path) {
+        Integer userId = userDetailsImpl.getId();
+        log.info("Received DELETE /api/resource request for user ID: {} and path: '{}'", userId, path);
+        resourceService.deleteResource(userDetailsImpl.getId(), path);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/download")
-    public ResponseEntity<Resource> downloadResource(@RequestParam Integer id, @RequestParam @NotBlank String path) {
-        ResourceDownloadResponse resource = resourceService.downloadResource(id, path);
+    public ResponseEntity<Resource> downloadResource(@AuthenticationPrincipal UserDetailsImpl userDetailsImpl,
+                                                     @RequestParam @NotBlank(message = "'path' must not be blank") String path) {
+        ResourceDownloadResponse resource = resourceService.downloadResource(userDetailsImpl.getId(), path);
+        Integer userId = userDetailsImpl.getId();
+        log.info("Received GET /api/resource/download request for user ID: {} and path: '{}'", userId, path);
         String fileName = resource.getFileName();
 
         ContentDisposition contentDisposition = ContentDisposition.attachment()
@@ -61,16 +72,20 @@ public class ResourceController {
     }
 
     @GetMapping("/move")
-    public ResponseEntity<ResourceResponse> moveResource(@RequestParam Integer id,
-                                                         @RequestParam @NotBlank String from,
-                                                         @RequestParam @NotBlank String to) {
-        ResourceResponse response = resourceService.moveResource(id, from, to);
+    public ResponseEntity<ResourceResponse> moveResource(@AuthenticationPrincipal UserDetailsImpl userDetailsImpl,
+                                                         @RequestParam @NotBlank(message = "'from' must not be blank") String from,
+                                                         @RequestParam @NotBlank(message = "'to' must not be blank") String to) {
+        Integer userId = userDetailsImpl.getId();
+        log.info("Received GET /api/resource/move request for user ID: {} from path: '{}' to path: '{}'", userId, from, to);
+        ResourceResponse response = resourceService.moveResource(userDetailsImpl.getId(), from, to);
         return ResponseEntity.ok().body(response);
     }
 
     @GetMapping("/search")
-    public ResponseEntity<List<ResourceResponse>> searchResource(@RequestParam Integer id,
-                                                                 @RequestParam @NotBlank String query) {
-        return ResponseEntity.ok(resourceService.searchResources(id, query));
+    public ResponseEntity<List<ResourceResponse>> searchResource(@AuthenticationPrincipal UserDetailsImpl userDetailsImpl,
+                                                                 @RequestParam @NotBlank(message = "'query' must not be blank") String query) {
+        Integer userId = userDetailsImpl.getId();
+        log.info("Received GET /api/resource/search request for user ID: {} with query: '{}'", userId, query);
+        return ResponseEntity.ok(resourceService.searchResources(userDetailsImpl.getId(), query));
     }
 }
