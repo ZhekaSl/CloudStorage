2e484c629fa2eeabbdc9b0d91ce4508802f54b37
Zheka Slepko <zhenya.slepko@gmail.com>
2025-03-26 18:44:32 +0200

    refactor: change regex in AuthRequest.

--- src/main/java/ua/zhenya/cloudstorage/dto/AuthRequest.java
+++ src/main/java/ua/zhenya/cloudstorage/dto/AuthRequest.java
@@ -10,11 +10,10 @@ import lombok.Data;
 @Builder
 public class AuthRequest {
     @NotBlank(message = "Username cannot be blank")
-    @Size(min = 3, max = 15, message = "Username must be between 3 and 15 characters")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
     @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "Username can only contain letters and numbers")
     private String username;
     @NotBlank(message = "Password cannot be blank")
-    @Size(min = 8, max = 64, message = "Password must be between 8 and 64 characters")
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$", message = "Password must contain at least one letter and one number")
+    @Size(min = 8, max = 128, message = "Password must be between 8 and 128 characters")
     private String password;
 }
